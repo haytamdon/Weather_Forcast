63ec0186c5cf75a1fbcb2a5d19d066d1c6a6ec76
Haitam Borqane <haytamborquane@gmail.com>
2022-09-28 12:00:35 +0100

    Logging params to comet ml

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -3,9 +3,10 @@ from tensorflow import keras
 from comet_ml import Experiment
 import logging
 
-def trainer(model, train, val, opt = "rmsprop", loss_func = "mse", metric_list = ["mae"], epoch_num= 50):
+def trainer(model, train, val, params, opt = "rmsprop", loss_func = "mse", metric_list = ["mae"], epoch_num= 50):
     exp=Experiment(project_name="Temperature_Forcast",
                     auto_histogram_gradient_logging=True)
+    exp.log_parameters(params)
     callbacks = [
         keras.callbacks.ModelCheckpoint("LSTM_model_best.keras",
                                         save_best_only=True)
@@ -16,3 +17,4 @@ def trainer(model, train, val, opt = "rmsprop", loss_func = "mse", metric_list =
                     validation_data=val,
                     callbacks=callbacks)
     return history
+
