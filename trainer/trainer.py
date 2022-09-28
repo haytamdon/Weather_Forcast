3bb7122c0c61d6deb08110a86b19023078d5298b
Haitam Borqane <haytamborquane@gmail.com>
2022-09-28 11:58:37 +0100

    Adding logging using comet ml

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 from tensorflow import keras
+from comet_ml import Experiment
+import logging
 
 def trainer(model, train, val, opt = "rmsprop", loss_func = "mse", metric_list = ["mae"], epoch_num= 50):
+    exp=Experiment(project_name="Temperature_Forcast",
+                    auto_histogram_gradient_logging=True)
     callbacks = [
         keras.callbacks.ModelCheckpoint("LSTM_model_best.keras",
                                         save_best_only=True)
