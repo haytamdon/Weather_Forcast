d529037d41098c39b7c9d8140f1254d38f1960fd
Haitam Borqane <haytamborquane@gmail.com>
2022-09-28 17:00:03 +0100

    adding a processing layer for inference data in the model

--- API server/server.py
+++ API server/server.py
@@ -12,6 +12,17 @@ class Features(BaseModel):
 model_file_path = "../../LSTM_model_best.keras"
 model = tf.keras.models.load_model(model_file_path)
 
+def process_data(X):
+    delay = 858
+    inference = keras.utils.timeseries_dataset_from_array(
+        X[:-delay],
+        targets=None,
+        sampling_rate= 6,
+        sequence_length= 120,
+        shuffle=True,
+        batch_size=batch_size,
+        start_index=num_train_samples + num_val_samples)
+    return inference
 
 app = FastAPI()
 @app.post("/run_model")
@@ -20,6 +31,7 @@ async def run_model(features: Features):
         'note': features.note,
         'list': features.data
         } )
+    
     y_pred = model.predict([features.data])
     print(Y_pred)
     return {
